37fe9b55ea505d8575e2c570dbcdf520a8321832
Ohad Doron <ogdoron@gmail.com>
2017-07-07 14:45:59 +0300

    add combin otm

--- RTS clustering validation/run_correlate_templates_spikes.m
+++ RTS clustering validation/run_correlate_templates_spikes.m
@@ -1,5 +1,9 @@
 fpath = 'D:\MATLAB\Results\m258r1\m258r1.17.18.19.20.21.22.23.24\40 minutes parts 30 minutes overlap';
 offset_val = 1;
+overlap_step = 10;
+max_time = 40;
+Fs = 20e3;
+
 
 files = dir(fpath);
 files(1:2) = [];
@@ -10,10 +14,37 @@ RTS_clu = cell(20,1);
 RTS_res = cell(20,1);
 nFolders = length(names);
 for i = 2 : nFolders
+    if i > 2 
+        fpath0 = fullfile(fpath,sprintf('%d',names(i-1)));
+        files = dir(fpath0);
+        name = {files.name};
+        templates_idx = ~cellfun(@isempty,strfind(name,'templates.mat'));
+        load(fullfile(fpath1,files(templates_idx).name));
+        templates0 = merged_templates;
+    end
     fpath1 = fullfile(fpath,sprintf('%d',names(i-1)));
     fpath2 = fullfile(fpath,sprintf('%d',names(i)));
-    [clu,res] = correlate_templates_spikes ( fpath1,fpath2,offset_val );
-    clu(1) = [];
-    RTS_clu{i} = clu;
-    RTS_res{i} = res;
+    
+    files = dir(fpath1);
+    name = {files.name};
+    templates_idx = ~cellfun(@isempty,strfind(name,'templates.mat'));
+    load(fullfile(fpath1,files(templates_idx).name));
+    templates1 = merged_templates;
+    
+    if i > 2
+        templates2remove = clusters(logical(clusters(:,i-2)) & logical(clusters(:,i-1)),i-2);
+        templates0(:,:,templates2remove) = -inf;
+    else
+        templates0 = ones(size(templates1(:,:,1))) * -inf;
+    end
+    
+        
+    
+
+    
+%     [clu,res] = correlate_templates_spikes (offset_val,...
+%         overlap,max_overlap,templates1, templates2);
+%     clu(1) = [];
+%     RTS_clu{i} = clu;
+%     RTS_res{i} = res;
 end
